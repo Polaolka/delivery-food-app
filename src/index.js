9a5908bdc9cf74bd771be3c8123b6a26ac1f633e
Olga Polishchuk <olgapolichuk@ukr.net>
2023-06-05 21:03:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Polaolka/Delivery-app.git">
+        <BrowserRouter basename="/delivery-food-app">
           <App />
         </BrowserRouter>
       </PersistGate>
